e732e7a40270ac4fb9b8ff29e81d9347d3703289
Julien <julien.molinier@etu.univ-cotedazur.fr>
2019-03-19 18:32:09 +0100

    Create frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
