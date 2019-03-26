5a4ad6dfad0e22ce1700321311b9503f5ff99faa
Unknown <youssef.mekouar@etu.unice.fr>
2019-03-26 10:08:18 +0100

    #5 modification et affichage des avis sur la page des avis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ResearchComponent} from "./researchComponents/research/research.component";
-import {HomeComponent} from "./home/home.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {ReviewFormComponent} from "./reviews/review-form";
+import {ResearchComponent} from './researchComponents/research/research.component';
+import {HomeComponent} from './home/home.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ReviewFormComponent} from './reviews/review-form';
+import {ReviewListComponent} from './reviews/review-list';
 
 const routes: Routes = [
-  {path: 'review-form', component: ReviewFormComponent},
+  {path: 'reviewForm', component: ReviewFormComponent},
+  {path: 'reviews' , component: ReviewListComponent},
   {path: 'research', component: ResearchComponent},
   {path: '', component: HomeComponent},
   {path: '**', component: PageNotFoundComponent},
---
+++ src/services/review.ts
@@ -0,0 +1,30 @@
+
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs/index';
+import {Review} from '../models/review';
+import { REVIEW_MOCKED } from '../mocks/review.mock';
+import {HttpClient} from '@angular/common/http';
+import {tick} from '@angular/core/testing';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+  private reviewList: Review[] = REVIEW_MOCKED;
+  public reviews$: BehaviorSubject<Review[]> = new BehaviorSubject(this.reviewList);
+  private url = 'http://localhost:9428';
+
+
+  constructor() {
+  }
+
+  /*
+  getReview() {
+    this.http.get<Review[]>(`${this.url}/api/review`).subscribe(value => {
+      this.reviewList = value;
+      this.reviews$.next(this.reviewList);
+      console.log(this.reviewList);
+    });
+  }
+*/
+}
