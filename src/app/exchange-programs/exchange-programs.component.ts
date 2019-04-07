df7d5d3d735cd4d60c70952077aac2a21eeda9f3
Hasnaa-Elalaoui <hasnaa.elalaoui-m'hamdy@etu.univ-cotedazur.fr>
2019-04-08 00:22:26 +0200

    #2 Exchange Programs: descriptions+images

--- src/app/exchange-programs/exchange-programs.component.ts
+++ src/app/exchange-programs/exchange-programs.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -7,6 +7,8 @@ import {Router} from '@angular/router';
   styleUrls: ['./exchange-programs.component.scss']
 })
 export class ExchangeProgramsComponent implements OnInit {
+  @Input()
+  exchange: ExchangeProgramsComponent;
 
   constructor(private router: Router) {
   }
