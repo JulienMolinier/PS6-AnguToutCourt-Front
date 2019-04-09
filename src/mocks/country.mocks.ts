ded0e84ef6dc926f5ce589610c869e78fb783736
Julien <julien.molinier@etu.univ-cotedazur.fr>
2019-04-09 09:46:15 +0200

    #3 ajout d'un filtre par pays

---
+++ src/mocks/country.mocks.ts
@@ -0,0 +1,32 @@
+import {Country} from '../models/country';
+
+export const COUNTRY_MOCKED: Country[] = [
+  {
+    name: 'Suède',
+    continent: 'Europe',
+  },
+  {
+    name: 'Angleterre',
+    continent: 'Europe',
+  },
+  {
+    name: 'Espagne',
+    continent: 'Europe',
+  },
+  {
+    name: 'Suède',
+    continent: 'Europe',
+  },
+  {
+    name: 'Canada',
+    continent: 'Amérique du Nord',
+  },
+  {
+    name: 'Etats-Unis',
+    continent: 'Amérique du Nord',
+  },
+  {
+    name: 'Chine',
+    continent: 'Asie',
+  }
+];
