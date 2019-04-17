2b7c732c473f533f34ce7d910b1969db4e40b6c0
Julien <julien.molinier@etu.univ-cotedazur.fr>
2019-04-17 12:35:31 +0200

    #3 ajout de filtres

--- src/app/research-components/research-result-list/research-result-list.component.ts
+++ src/app/research-components/research-result-list/research-result-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {University} from '../../../models/university';
 import {UniversityService} from '../../../services/universityService';
 import {Router} from '@angular/router';
-import {BehaviorSubject} from 'rxjs';
 import {COUNTRY_MOCKED} from '../../../mocks/country.mocks';
 import {Country} from '../../../models/country';
 import {EXCHANGE_MOCKED} from '../../../mocks/exchange.mocks';
@@ -18,21 +17,15 @@ export class ResearchResultListComponent implements OnInit {
   private countryList: Country[];
   private exchangeProgList: string[];
   private semesterList: string[];
-  private rateCheckbox: BehaviorSubject<boolean>;
-  private placeCheckbox: BehaviorSubject<boolean>;
-  private oldCheckbox: BehaviorSubject<boolean>;
-  private countryFilter: string;
-  private exchangeFilter: string;
-  private semesterFilter: string;
+  private check: string;
+  private filters: [string, string, string];
 
   constructor(public universityService: UniversityService, private router: Router) {
     this.researchResultList = [];
+    this.filters = [null, null, null];
     this.countryList = COUNTRY_MOCKED;
     this.exchangeProgList = EXCHANGE_MOCKED;
-    this.semesterList = ['S1', 'S2', 'S1 et S2'];
-    this.rateCheckbox = new BehaviorSubject<boolean>(false);
-    this.placeCheckbox = new BehaviorSubject<boolean>(false);
-    this.oldCheckbox = new BehaviorSubject<boolean>(false);
+    this.semesterList = ['1', '2', '1 et 2'];
     universityService.getUniversities();
     this.getUniversitiesList();
   }
@@ -42,30 +35,15 @@ export class ResearchResultListComponent implements OnInit {
 
 
   onFilterRateChange() {
-    this.rateCheckbox.next(!this.rateCheckbox.getValue());
-    if (this.rateCheckbox.getValue()) {
-      this.researchResultList.sort((a, b) => a.rate > b.rate ? -1 : 1);
-    } else {
-      this.researchResultList.sort((a, b) => a.rate > b.rate ? 1 : -1);
-    }
+    this.researchResultList.sort((a, b) => a.rate > b.rate ? -1 : 1);
   }
 
   onFilterPlaceChange() {
-    this.placeCheckbox.next(!this.placeCheckbox.getValue());
-    if (this.placeCheckbox.getValue()) {
-      this.researchResultList.sort((a, b) => a.placesNumber > b.placesNumber ? -1 : 1);
-    } else {
-      this.researchResultList.sort((a, b) => a.placesNumber > b.placesNumber ? 1 : -1);
-    }
+    this.researchResultList.sort((a, b) => a.placesNumber > b.placesNumber ? -1 : 1);
   }
 
   onFilterOldChange() {
-    this.oldCheckbox.next(!this.oldCheckbox.getValue());
-    if (this.oldCheckbox.getValue()) {
-      this.researchResultList.sort((a, b) => a.nbOldExp > b.nbOldExp ? -1 : 1);
-    } else {
-      this.researchResultList.sort((a, b) => a.nbOldExp > b.nbOldExp ? 1 : -1);
-    }
+    this.researchResultList.sort((a, b) => a.nbOldExp > b.nbOldExp ? -1 : 1);
   }
 
   navigateUniversityDetails(university: University) {
@@ -76,50 +54,27 @@ export class ResearchResultListComponent implements OnInit {
     this.universityService.universities$.subscribe((value) => this.researchResultList = value);
   }
 
-  onCountryFilterChange() {
-    if (this.countryFilter) {
-      this.getUniversitiesList();
-      this.researchResultList = this.researchResultList.filter(value => value.country === this.countryFilter);
-    } else {
-      this.getUniversitiesList();
-    }
-  }
-
-  onExchangeFilterChange() {
-    if (this.exchangeFilter) {
-      this.getUniversitiesList();
-      this.researchResultList = this.researchResultList.filter(value =>
-        value.program.toLowerCase() === this.exchangeFilter.toLowerCase());
-    } else {
-      this.getUniversitiesList();
-    }
-  }
-
-  onSemesterFilterChange() {
-    if (this.semesterFilter === 'S1') {
-      this.getUniversitiesList();
-      this.researchResultList = this.researchResultList.filter(value =>
-        value.semester.includes(1));
-    } else if (this.semesterFilter === 'S2') {
-      this.getUniversitiesList();
-      this.researchResultList = this.researchResultList.filter(value =>
-        value.semester.includes(2));
-    } else if (this.semesterFilter === 'S1 et S2') {
-      this.getUniversitiesList();
-      this.researchResultList = this.researchResultList.filter(value =>
-        value.semester.includes(1) && value.semester.includes(2));
-    } else {
-      this.getUniversitiesList();
+  onChangeFilter() {
+    this.getUniversitiesList();
+    for (let i = 0; i <= this.filters.length; i++) {
+      if (i === 0 && (this.filters[i] === '1' || this.filters[i] === '2')) {
+        this.researchResultList = this.researchResultList.filter(value =>
+          value.semester.includes(Number(this.filters[i])));
+      } else if (i === 0 && this.filters[i] === '1 et 2') {
+        this.researchResultList = this.researchResultList.filter(value =>
+          value.semester.includes(1) && value.semester.includes(2));
+      } else if (i === 1 && this.filters[i] !== null) {
+        this.researchResultList = this.researchResultList.filter(value =>
+          value.program.toLowerCase() === this.filters[i].toLowerCase());
+      } else if (i === 2 && this.filters[i] !== null) {
+        this.researchResultList = this.researchResultList.filter(value => value.country === this.filters[i]);
+      }
     }
   }
 
   resetResearchList() {
+    this.check = '';
+    this.filters = [null, null, null];
     this.getUniversitiesList();
-    this.rateCheckbox = new BehaviorSubject<boolean>(false);
-    this.placeCheckbox = new BehaviorSubject<boolean>(false);
-    this.oldCheckbox = new BehaviorSubject<boolean>(false);
-    this.countryFilter = null;
-    this.semesterFilter = null;
-    this.exchangeFilter = null;
   }
 }
