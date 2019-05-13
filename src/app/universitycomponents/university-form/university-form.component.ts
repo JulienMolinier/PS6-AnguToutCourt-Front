c01dbf7d5274715dafc61b03ebe217cdb890b5f6
Julien <julien.molinier@etu.univ-cotedazur.fr>
2019-05-13 23:27:19 +0200

    #10 university form

--- src/app/universitycomponents/university-form/university-form.component.ts
+++ src/app/universitycomponents/university-form/university-form.component.ts
@@ -12,40 +12,45 @@ import {EXCHANGE_MOCKED} from '../../../mocks/exchange.mocks';
 export class UniversityFormComponent implements OnInit {
 
   private universityForm: FormGroup;
-  private semesterList: string[];
+  private semesterList: number[];
   private exchangeProgList: string[];
   private campusImgsPath: string[];
+  private majorList: string[];
+
+  private contactsRaw: string;
+  private campusImgsRaw: string;
 
   constructor(private formBuilder: FormBuilder,
               private universityService: UniversityService) {
 
-    this.semesterList = ['1', '2'];
+    this.semesterList = [1, 2];
     this.campusImgsPath = [];
+    this.majorList = ['GE', 'GB', 'SI', 'BAT', 'MAM'];
     this.exchangeProgList = EXCHANGE_MOCKED;
+    this.contactsRaw = '';
+    this.campusImgsRaw = '';
+
     this.universityForm = this.formBuilder.group({
       name: ['', [Validators.required]],
-      img: ['', [Validators.required]],
+      imgPath: ['', [Validators.required]],
       country: ['', [Validators.required]],
       city: ['', [Validators.required]],
       program: ['', [Validators.required]],
       placesNumber: ['', [Validators.required]],
       description: ['', [Validators.required]],
       nbOldExp: ['', [Validators.required]],
-      semester: ['', [Validators.required]],
+      semester: [[], [Validators.required]],
       campusDesc: ['', [Validators.required]],
-      campusImgs: ['', [Validators.required]],
       location: ['', [Validators.required]],
-      inscriptionDesc: ['', [Validators.required]],
-      contacts: ['', [Validators.required]],
-      major: ['', [Validators.required]],
+      inscriptiondesc: ['', [Validators.required]],
+      major: [[], [Validators.required]],
       link: ['', [Validators.required]],
     });
+
   }
 
   ngOnInit(): void {
   }
-
-  addCustom = (term) => ({id: term, name: term});
 
   addUniversity() {
     if (this.universityForm.valid) {
@@ -53,9 +58,20 @@ export class UniversityFormComponent implements OnInit {
       university.nbReviews = 0;
       university.rate = 0;
       university.rates = [];
+      university.recommended = false;
+      university.campusImgs = this.campusImgsRaw.split("\n");
+      university.contacts = this.contactsRaw.split("\n");
       this.universityService.postUniversity(university);
     } else {
+      console.log("Invalid");
     }
   }
 
+  contactChange(newValue) {
+    this.contactsRaw = newValue;
+  }
+
+  campusImgChange(newValue) {
+    this.campusImgsRaw = newValue;
+  }
 }
